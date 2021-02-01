d8c334a838af2deec7a52a9ec1bf8521ba16e19b
tuki7 <78048544+tuki7@users.noreply.github.com>
2021-02-01 01:37:27 -0300

    Add files via upload

---
+++ li/belle.js
@@ -0,0 +1,10 @@
+exports.belle = belle() // Isso explica sobre a Íris, modifique ou não se quiser
+function belle() {
+	return `*LA CONOCES???*
+
+_Uyayaiii!! es una belleza NOOO?, *Quieres mas fotos como estas? comunicate con 👑SAMU👑*._
+
+_*PERO TIENES QUE ESTAR SUSCRITO ASU CANAL!!🥴*_
+
+https://www.youtube.com/channel/UCqPXxG2ZdDe_ugOqMHDRMqg`
+}
